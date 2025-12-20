4beb15e26d6edb7d194d0daaf4a9b69b3c721449
Edu Lukmon <edulukmon@gmail.com>
2025-12-20 21:56:44 +0100

    finished learning OOP

--- MiniProject/OOp.py
+++ MiniProject/OOp.py
@@ -26,16 +26,57 @@ class BankAccount:
         print(f'Bank ID: {self.name}, Balance: {self.balance}')
 
     @classmethod
-    def change_name(cls, new_name):
+    def change_name(cls, new_name, open_account):
         cls.bank_name = new_name
+        cls.open_account = open_account
 
     @classmethod
     def change_size(cls):
         cls.account_size += 5
 
+    @staticmethod
+    def show_bank_name():
+        return BankAccount.bank_name
+
+    @staticmethod
+    def add_money(amount):
+        return f'{amount} added to account'
+
 lammycoal = BankAccount('Lammy', 22)
 edu = BankAccount('Edu', 23)
+
+
+
+BankAccount.open_account = 23
+print(BankAccount.show_bank_name())
+print(BankAccount.add_money(10))
 print(BankAccount.account_size)
-print(BankAccount.change_name('access'))
+print(BankAccount.change_name('access','23'))
 print(lammycoal.balance)
 print(lammycoal.bank_name)
+
+class ATM:
+    limit = 10000
+
+    def __init__(self, name):
+        self.name = name
+
+    @classmethod
+    def daily(cls, new_limit):
+        if new_limit <= 0:
+            print ("new limit cannot be zero or negative")
+            return
+        cls.new_limit = ATM.limit
+        print(f"Today's limit is {cls.new_limit}")
+
+    @staticmethod
+    def is_valid_amount(amount):
+        if not isinstance(amount, int):
+            return False
+        if amount <= 0:
+            return False
+        return True
+
+ATM.daily(100)
+ATM.is_valid_amount(100)
+
